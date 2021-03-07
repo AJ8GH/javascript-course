7ba296fff3d3596649133e297498730213b265f8
Adam Jonas <adamjonas25@gmail.com>
2021-03-07 22:07:40 +0000

    control flow

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,3 +54,67 @@ also
 multiline`
 
 console.log(otherString)
+
+const averagePopulation = 33000000
+const ukPopulation = 66000000
+// if else statement structure in javascript
+if (ukPopulation > averagePopulation) {
+  console.log(`The UK's population of ${ukPopulation} is ${ukPopulation - averagePopulation} above the average`)
+} else {
+  console.log(`that's hella low`)
+}
+
+// type conversion
+
+const inputYear = '1991';
+
+console.log(inputYear)
+console.log(Number(inputYear));
+console.log(Number('Jonas'));
+// outputs Nan -- Not a Number - not a number is still type number.... lol
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old')
+// + operator coerces the strings and numbers to convert together
+console.log('10' + 23 - '2')
+console.log(10 - '23' - '2')
+// minus minues the value, + operator concatenates the values
+// minus converts string to number
+// plus converts number to string
+
+// 6 falsey values in js
+// 0, '', undefined, null, NaN, false
+// everything else is truthy
+console.log(Boolean(0));
+console.log(Boolean('0'));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean([]));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(false));
+
+let adam
+
+if (adam) {
+  console.log('Adam is defined!')
+} else {
+  console.log('Not defined!!!!!')
+}
+
+const age = 18;
+if (age === 18) console.log('18!')
+// triple === - strict equality operator - performs type coercion
+// double == - loose equality operator - does not perform type coercion
+
+console.log(18 == '18')
+console.log(18 === '18')
+console.log(18 !== '18')
+
+const favourite = Number(prompt("What's your favourite number?"));
+
+console.log(favourite)
+
+const javaScriptFun = true
